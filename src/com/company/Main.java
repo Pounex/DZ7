fd4fd73917074ea58f8ddfdc9a1fdf45cdbf9726
asus <nik.kardymon@mail.ru>
2021-04-20 19:02:31 +0300

    second commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,52 @@
 package com.company;
 
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Person pr1 = new Person("Alex", "Makarenko", Gender.FEMALE);
+        Person pr2 = new Person("Anna", "Vorobyova", Gender.FEMALE);
+        Person pr3 = new Person("Innokentiy", "Kalitka", Gender.MALE);
+        Person pr4 = new Person("Sasha", "Makarenko", Gender.MALE);
+        Person pr5 = new Person("Sasha", "Makarenko", Gender.MALE);
+        List<Person> list = new ArrayList<>();
+
+        list.add(pr1);
+        list.add(pr2);
+        list.add(pr3);
+        list.add(pr4);
+        list.add(pr5);
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+        System.out.println("Количество элементов :" + list.size());
+
+        Set<Person> set = new HashSet<>();
+        set.add(pr1);
+        set.add(pr2);
+        set.add(pr3);
+        set.add(pr4);
+        set.add(pr5);
+
+        for (Person item : set) {
+            System.out.println(item);
+
+        }
+        System.out.println("Количество элементов :" +set.size());
+
+        Map<Integer,Person> map = new HashMap<>();
+        map.put(1,pr1);
+        map.put(2,pr2);
+        map.put(3,pr3);
+        map.put(4,pr4);
+        map.put(5,pr5);
+
+        for(Map.Entry<Integer,Person> pair : map.entrySet())
+        {
+            System.out.println(pair.getValue()+ " "+  pair.getKey());
+        }
+        System.out.println(map.size());
+
     }
 }
---
+++ src/com/company/Person.java
@@ -0,0 +1,50 @@
+package com.company;
+
+import java.util.Objects;
+
+public final class Person {
+    private final String firstName;
+    private final String lastName;
+    private final Gender gender;
+
+    public Person(String firstName, String lastName, Gender gender) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return Objects.equals(firstName, person.firstName) && Objects.equals(lastName, person.lastName) && gender == person.gender;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName, gender);
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", gender=" + gender +
+                '}';
+    }
+}
+
